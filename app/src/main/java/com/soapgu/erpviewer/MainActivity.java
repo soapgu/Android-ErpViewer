7230808004b2a4cfb98920e9e8c6663a786f0aca
soapgu <ghost_gu@163.com>
2022-05-10 20:50:31 +0800

    complete get erp tasks

--- app/src/main/java/com/soapgu/erpviewer/MainActivity.java
+++ app/src/main/java/com/soapgu/erpviewer/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.orhanobut.logger.Logger;
 import com.soapgu.erpviewer.restful.Api;
 
@@ -18,18 +21,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String domain = "erp.shgbit.com";
     private Api api;
     private TextView message;
     private final CompositeDisposable disposables = new CompositeDisposable();
@@ -41,14 +50,25 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void saveFromResponse(@NonNull HttpUrl url, @NonNull List<Cookie> cookies) {
                         cookies.forEach( t-> Logger.i("save cookie:%s",t.toString()) );
-                        cookieStore.put(url.host(), cookies);
+                        Logger.i( "save cookie host:%s,count %s",url.host(),cookies.size() );
+                        List<Cookie> saveCookies = cookieStore.get(url.host());
+                        if( saveCookies == null ){
+                            saveCookies = new ArrayList<>(cookies);
+                        }
+                        else{
+                            saveCookies.addAll( cookies );
+                        }
+                        cookieStore.put(url.host(), saveCookies);
                     }
 
                     @NonNull
                     @Override
                     public List<Cookie> loadForRequest(@NonNull HttpUrl url) {
                         List<Cookie> cookies = cookieStore.get(url.host());
-                        return cookies != null ? cookies : new ArrayList<>();
+
+                        List<Cookie> retValue = cookies != null ? cookies : new ArrayList<>();
+                        Logger.i( "load cookie count: %s",retValue.size() );
+                        return retValue;
                     }
                 }).build();
         String baseUrl = "https://erp.shgbit.com/";
@@ -73,23 +93,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onLoginClick(){
+        cookieStore.clear();
         disposables.add( this.api.getItCode()
-                .map( t->{
+                .flatMapMaybe( t->{
                     Document doc = Jsoup.parse(t.string());
                     Elements elements = doc.getElementsByAttributeValue("name","lt");
                     if( !elements.isEmpty() ) {
-                       return  Objects.requireNonNull(elements.first()).attr("value");
+                       return Maybe.just(Objects.requireNonNull(elements.first()).attr("value") );
                     }
-                    return "";
+                    return Maybe.empty();
                 } )
+                .flatMap( t -> this.api.login( t, "guhui", "******", "e1s1","submit" )
+                        .flatMapMaybe( r->{
+                            Pattern pattern = Pattern.compile("(?<=_userInfo=).*(?=;)");
+                            Matcher matcher = pattern.matcher(r.string());
+                            if( matcher.find() ){
+                                JsonArray jsonArray = new JsonParser().parse(matcher.group()).getAsJsonArray();
+                                addLoginCodeCookie( jsonArray.get(0).getAsJsonObject().get("logonCode").getAsString() );
+                                return Maybe.just(true);
+                            }
+                            return Maybe.empty();
+                        } ))
+                .flatMapSingle( t -> this.api.getTasks( "loadTaskListJSON" )
+                        .map(ResponseBody::string))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe( t->{
-                            Logger.i( "it code:%s",t );
-                            this.message.setText(String.format("response ok:%s", t));
+                            Logger.i( "login response:%s",t );
+                            this.message.setText(String.format("response ok:%s", "ok"));
                         },
                         e ->{
-                            Logger.e( e, "error with get itCode" );
+                            Logger.e( e, "error with login" );
                             this.message.setText( String.format("response error:%s",e.getMessage()));
                         }) );
+    }
+
+    private void addLoginCodeCookie( String loginCode ){
+        Objects.requireNonNull(cookieStore.get(domain))
+                .add( new Cookie.Builder()
+                    .name("_CUNAME")
+                    .value(loginCode)
+                    .path("/jqerp")
+                    .domain(domain)
+                    .build());
     }
 }
--- app/src/main/java/com/soapgu/erpviewer/restful/Api.java
+++ app/src/main/java/com/soapgu/erpviewer/restful/Api.java
@@ -2,10 +2,21 @@ package com.soapgu.erpviewer.restful;
 
 import io.reactivex.rxjava3.core.Single;
 import okhttp3.ResponseBody;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 
 public interface Api {
     @GET("jqerp")
     Single<ResponseBody> getItCode();
+
+    @FormUrlEncoded
+    @POST("cas/login?service=https://erp.shgbit.com/jqerp/")
+    Single<ResponseBody> login(@Field("lt") String lt,@Field("username") String username, @Field("password") String password, @Field("execution") String execution,@Field("_eventId") String _eventId);
+
+    @FormUrlEncoded
+    @POST("jqerp/web/taskInfoService")
+    Single<ResponseBody> getTasks(@Field("actType") String actType);
 }
